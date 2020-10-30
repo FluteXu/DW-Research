be6147de725fac7dae46b1413eec67350ae4a955
wangcheng <wangcheng@deepwise.com>
2020-10-30 15:42:28 +0800

    rm pyc

---
+++ train_1x_57.sh
@@ -0,0 +1,2 @@
+python ./tools/train_net_vov.py --num-gpus 8 \
+--config-file configs/vovnet/mask_rcnn_V_57_FPN_1x.yaml
---
+++ train_1x_99.sh
@@ -0,0 +1,2 @@
+python ./tools/train_net_vov.py --num-gpus 8 \
+--config-file configs/vovnet/mask_rcnn_V_99_FPN_1x.yaml
