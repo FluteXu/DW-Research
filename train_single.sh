32f5b10f20bc8e977eaec8b9af7e99226c55824d
wangcheng <wangcheng@deepwise.com>
2020-10-30 15:02:24 +0800

    3dce update

--- train_single.sh
+++ train_single.sh
@@ -1,4 +1,5 @@
 CUDA_VISIBLE_DEVICES=1 python ./tools/train_net_vov.py \
---config-file configs/vovnet/mask_rcnn_V_39_FPN_3x.yaml \
+--config-file configs/vovnet/mask_rcnn_V_57_FPN_1x_3dce.yaml \
 --num-gpus 1 SOLVER.IMS_PER_BATCH 2 \
-DATALOADER.NUM_WORKERS 1 VIS_PERIOD 10
+DATALOADER.NUM_WORKERS 1
+#VIS_PERIOD 10
